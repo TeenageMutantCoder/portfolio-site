a5ca7d9407cd2baff1a687c3c4d7de9db07e5bd5
Stevon Wright <stevonisepic@gmail.com>
2021-02-01 13:07:53 -0500

    Changed styling, added svg files

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,4 @@
-// Import Header and Footer into page
+// To import header and footer into page
 let header = document.querySelector("header");
 let footer = document.querySelector("footer");
 
@@ -10,5 +10,21 @@ function importHTML(source, destination) {
     });
 }
 
-importHTML("/common/navbar.html", header);
+importHTML("/common/header.html", header);
 importHTML("/common/footer.html", footer);
+
+// To allow to open and close navbar menu
+let hamburger = document.getElementById("#header__hamburger");
+let menu = document.getElementById("#header__menu");
+
+function addCollapsibleMenu(button, menu) {
+    button.addEventListener("click", () => {
+        if ("open" in menu.classList) {
+            menu.classList.remove("open");
+        } else {
+            menu.classList.add("open");
+        }
+    });
+}
+
+addCollapsibleMenu(hamburger, menu);
