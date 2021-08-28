e68a70c0d0ea8f897dbeef6f4cfc7888cae3a29b
Stevon Wright <stevonisepic@gmail.com>
2021-08-27 23:56:47 -0400

    Added TXT resume file/download button, made buttons take 100% width

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,31 +5,37 @@ import Button from "./button";
 import "./resume.scss";
 
 const Resume = () => {
+    const resumeFileName = "StevonWrightResume";
+
     return (
         <section className="Resume">
             <iframe
                 title="My Resume/CV"
                 src="https://drive.google.com/file/d/1PU5yowoZuFg9fjMt5ny0ETVZWEu4fsbz/preview"
             ></iframe>
-            <div className="Resume__buttons">
-                <Button
-                    type="button"
-                    text="Print"
-                    onClick={() => printJS("/Resume.pdf")}
-                />
-                <Button
-                    type="link"
-                    text="Download as PDF"
-                    href="/Resume.pdf"
-                    download="Stevon-Wright"
-                />
-                <Button
-                    type="link"
-                    text="Download as DOCX"
-                    href="/Resume.docx"
-                    download="Stevon-Wright"
-                />
-            </div>
+            <Button
+                type="button"
+                text="Print"
+                onClick={() => printJS("/Resume.pdf")}
+            />
+            <Button
+                type="link"
+                text="Download as PDF"
+                href="/Resume.pdf"
+                download={resumeFileName}
+            />
+            <Button
+                type="link"
+                text="Download as DOCX"
+                href="/Resume.docx"
+                download={resumeFileName}
+            />
+            <Button
+                type="link"
+                text="Download as TXT"
+                href="/Resume.txt"
+                download={resumeFileName}
+            />
         </section>
     );
 };
