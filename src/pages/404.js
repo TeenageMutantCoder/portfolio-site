3461d4ddf8821abc04b3b46b3b1f948d800a78f2
Stevon Wright <stevonisepic@gmail.com>
2021-09-07 15:42:07 -0400

    Customized 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,52 @@
-import * as React from "react"
+import * as React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+import "../components/button.scss";
 
 const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+    <Layout>
+        <Seo title="404: Not found" />
+        <section
+            style={{
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+                height: "80vh",
+                justifyContent: "center",
+                margin: "0 auto",
+                width: "90vw",
+            }}
+        >
+            <h1
+                style={{
+                    fontSize: "4rem",
+                    textAlign: "center",
+                    marginBottom: "5rem",
+                }}
+            >
+                404: Not Found
+            </h1>
+            <p
+                style={{
+                    fontSize: "2rem",
+                    textAlign: "center",
+                    marginBottom: "5rem",
+                }}
+            >
+                You just hit a route that doesn&#39;t exist... the sadness.
+            </p>
+            <Link
+                style={{ width: "60%", marginBottom: "3rem" }}
+                className="Button"
+                to="/"
+            >
+                Go to Home
+            </Link>
+        </section>
+    </Layout>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
