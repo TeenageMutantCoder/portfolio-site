e267c586630a40dbefc363ef6df4c8ce815a58ab
Stevon Wright <stevonisepic@gmail.com>
2021-09-08 17:27:28 -0400

    Added simple GSAP animation to main

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 import Header from "./header";
 import Footer from "./footer";
 import "./layout.scss";
+import { gsap } from "gsap";
 
 const Layout = ({ children }) => {
     const links = [
@@ -12,11 +13,21 @@ const Layout = ({ children }) => {
         { name: "Resume/CV", path: "/resume/" },
         { name: "Contact", path: "/contact/" },
     ];
-
+    const mainRef = useRef();
+    useLayoutEffect(() => {
+        gsap.from(mainRef.current, {
+            opacity: 0.3,
+            y: 40,
+            duration: 0.8,
+            onComplete: () => {
+                gsap.to(mainRef.current, { clearProps: "all" }); // Used to remove transform property that messed up mobile menu
+            },
+        });
+    });
     return (
         <>
             <Header links={links} />
-            <main>{children}</main>
+            <main ref={mainRef}>{children}</main>
             <Footer links={links} />
         </>
     );
